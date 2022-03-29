bde4a1209658242592dd544744031baaafa5272b
whoyayawho <51109071+whoyayawho@users.noreply.github.com>
2022-03-29 19:53:52 +0900

    Add files via upload

---
+++ docker_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USER_NAME=smkang
+CONTAINER_NAME=ros2_dashing_cartographer
+IMAGE_NAME=soslab_msd2:ros2_dashing
+
+docker run --name=${CONTAINER_NAME} --ipc=host --net=host --privileged -it \
+--gpus all \
+-e DISPLAY=$DISPLAY \
+-e NVIDIA_VISIBLE_DEVICES=all \
+-e NVIDIA_DRIVER_CAPABILITIES=all \
+-e QT_X11_NO_MITSHM=1 \
+-e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+-v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native:rw \
+-v /home/${USER_NAME}/.config/pulse/cookie:/root/.config/pulse/cookie \
+-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+-v /dev:/dev:rw \
+${IMAGE_NAME} /bin/bash
