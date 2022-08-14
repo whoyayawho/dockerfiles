2664fb575e66be0b7f323da0efd99e76563aea2d
smkang0521 <sungmo.kang@soslab.co>
2022-08-14 22:20:29 +0900

    update vim install

--- docker_run.sh
+++ docker_run.sh
@@ -5,12 +5,14 @@ IMAGE_NAME=smkang0521/ros:dashing
 
 docker run --name=${CONTAINER_NAME} --ipc=host --net=host --privileged -it \
 --gpus all \
+--device /dev/snd \
 -e DISPLAY=$DISPLAY \
 -e NVIDIA_VISIBLE_DEVICES=all \
 -e NVIDIA_DRIVER_CAPABILITIES=all \
 -e QT_X11_NO_MITSHM=1 \
 -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
--v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native:rw \
+-v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
 -v /home/${USER}/.config/pulse/cookie:/root/.config/pulse/cookie \
--v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+--group-add $(getent group audio | cut -d: -f3) \
 ${IMAGE_NAME} /bin/bash
